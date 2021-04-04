67cbde38efd99caa27865f11772ebe5452eee2d3
aashrith-sai <47492639+aashrith-sai@users.noreply.github.com>
2021-04-03 21:18:36 -0500

    Update policy_factory.py

--- crowd_nav/policy/policy_factory.py
+++ crowd_nav/policy/policy_factory.py
@@ -2,7 +2,12 @@ from crowd_sim.envs.policy.policy_factory import policy_factory
 from crowd_nav.policy.cadrl import CADRL
 from crowd_nav.policy.lstm_rl import LstmRL
 from crowd_nav.policy.sarl import SARL
+from crowd_nav.policy.sarl1 import SARL
+
+
+
 
 policy_factory['cadrl'] = CADRL
 policy_factory['lstm_rl'] = LstmRL
 policy_factory['sarl'] = SARL
+policy_factory['sarl1'] = SARL
